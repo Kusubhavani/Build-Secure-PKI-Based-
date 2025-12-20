30a7721bb9e20356f137fee4281a3871f116e3bd
Kusu Bhavani <bhavanikusu92@gmail.com>
2025-12-20 11:42:03 +0530

    Update decrypt_seed.py

--- decrypt_seed.py
+++ decrypt_seed.py
@@ -10,7 +10,6 @@ def decrypt_seed(encrypted_seed_b64: str, private_key):
         encrypted_seed_b64 += "=" * (4 - missing_padding)
 
     ciphertext = base64.b64decode(encrypted_seed_b64)
-
     plaintext = private_key.decrypt(
         ciphertext,
         padding.OAEP(
@@ -19,29 +18,20 @@ def decrypt_seed(encrypted_seed_b64: str, private_key):
             label=None
         )
     )
-
     seed = plaintext.decode("utf-8")
-
     if len(seed) != 64 or any(c not in "0123456789abcdef" for c in seed.lower()):
         raise ValueError("Invalid seed format")
-
     return seed
-
 
 def load_private_key(path="student_private.pem"):
     with open(path, "rb") as f:
         return serialization.load_pem_private_key(f.read(), password=None)
 
-
 if __name__ == "__main__":
     private_key = load_private_key()
-
     with open("encrypted_seed.txt", "r") as f:
         encrypted_seed = f.read()
-
     seed = decrypt_seed(encrypted_seed, private_key)
-
     with open("seed.txt", "w") as f:
         f.write(seed)
-
     print("Decrypted seed:", seed)
